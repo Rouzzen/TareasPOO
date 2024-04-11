f7c95a3e4163a1fd16bc7a1090f87381d81b1114
Shomuted <144641365+Shomuted@users.noreply.github.com>
2024-04-10 20:34:38 -0400

    Add files via upload

--- Item.java
+++ Item.java
@@ -1,9 +1,43 @@
 public abstract class Item {
+    protected String nombre;
 
-
-    public abstract void printItemAplicado();
-
-    /* Completar código de la clase */
+    public Item(String nombre){
+        this.nombre = nombre;
+    }
+    public abstract void printItemAplicado(Mascota mascota);
 }
 
+public class Comida extends Item {
+    public Comida(String nombre) {
+        super(nombre);
+    }
 
+    @Override
+    public void printItemAplicado(Mascota mascota) {
+        mascota.incrementarSalud(20);
+        mascota.incrementarEnergia(20);
+        System.out.println("Dando de comer: " + nombre);
+    }
+}
+public class Medicina extends Item {
+    public Medicina(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void printItemAplicado(Mascota mascota) {
+        mascota.incrementarSalud(40);
+        System.out.println("Aplicando Medicamento: " + nombre);
+    }
+}
+public class Juguete extends Item {
+    public Juguete(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void printItemAplicado(Mascota mascota) {
+        mascota.incrementarFelicidad(30);
+        System.out.println("Usando Juguete: " + nombre);
+    }
+}
