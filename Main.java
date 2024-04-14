cd61f97b532c29725c2873fb6c7c36d81b25e39b
dmoyan0 <diego.moyano@usm.cl>
2024-04-14 00:01:16 -0400

    asd

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
+    private Mascota mascota;
+    private Inventario inventario;
     public static void main(String[] args) throws IOException {
         // Carga de archivo config.csv
         if (args.length != 1) {
@@ -15,6 +17,7 @@ public class Main {
         Main stage1 = new Main();
         // Lectura de archivo config.csv
         stage1.readConfiguration(in);
+        stage1.printEstado(0, null);
 
         stage1.executeAction(new Scanner(System.in), System.out);
     }
@@ -35,16 +38,16 @@ public class Main {
             int cantidad = Integer.parseInt(item_csv[3]);
             
             if (tipoItem.equals("Comida")) {
-                System.out.println("Se agregó la comida");
+                System.out.println("Se agrego la comida: " + nombreItem);
                 inventario.agregarItem(new Comida(id, nombreItem, cantidad));
             } else if (tipoItem.equals("Medicina")) {
-                System.out.println("Se agregó la medicina");
+                System.out.println("Se agrego la medicina: "+ nombreItem);
                 inventario.agregarItem(new Medicina(id, nombreItem, cantidad));
             } else if (tipoItem.equals("Juguete")) {
-                System.out.println("Se agregó el juguete");
+                System.out.println("Se agrego el juguete: "+ nombreItem);
                 inventario.agregarItem(new Juguete(id, nombreItem, cantidad));
             } else {
-                System.out.println("Tipo de ítem desconocido: " + tipoItem);
+                System.out.println("Tipo de item desconocido: " + tipoItem);
             }
         }
     }
@@ -52,15 +55,21 @@ public class Main {
     public void executeAction(Scanner in, PrintStream out){
         /* Completar código con manejo de acciones y menú en las etapas que corresponda
         e incremento del tiempo dependiendo de la etapa */
+
     
     }
 
     public void printEstado(float step, PrintStream out){
         /* Completar método que muestra el estado de la Mascota y del inventario
         dependiendo de la etapa */
-
+        System.out.println("Parametros");
+        System.out.println("-----------");
+        System.out.println("Nombre: "+ mascota.getNombre());
+        System.out.println("Edad: "+ mascota.getEdad());
+        System.out.println("Salud: "+ mascota.getSalud());
+        System.out.println("Energia: "+ mascota.getEnergia());
+        System.out.println("Felicidad : "+ mascota.getFelicidad());
+        mascota.getEstado();
     }
-
-    private Mascota mascota;
-    private Inventario inventario;
+    
 }
